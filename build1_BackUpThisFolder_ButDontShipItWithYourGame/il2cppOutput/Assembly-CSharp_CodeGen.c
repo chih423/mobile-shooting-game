0c5774de81739286e4acbe55efb8eb05dd3ecb49
Mandy <apple2263@gmail.com>
2022-12-26 22:44:17 +0800

    設定

---
+++ build1_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,329 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Single Joystick::get_Horizontal()
+extern void Joystick_get_Horizontal_m78CF4472B86063E54254AC8AE0A52126E4008AFA (void);
+// 0x00000002 System.Single Joystick::get_Vertical()
+extern void Joystick_get_Vertical_mA2B0917896CF9CE47A6D342D1734E43441C3D4BE (void);
+// 0x00000003 UnityEngine.Vector2 Joystick::get_Direction()
+extern void Joystick_get_Direction_m52502695D42BDAB6075089BDE442ABE72EAC81EC (void);
+// 0x00000004 System.Single Joystick::get_HandleRange()
+extern void Joystick_get_HandleRange_mB38F0D3B6ADE2D1557D7A3D6759C469F17509D77 (void);
+// 0x00000005 System.Void Joystick::set_HandleRange(System.Single)
+extern void Joystick_set_HandleRange_m686B579A1F02EFCD4878BEA27EA606FC23CD2505 (void);
+// 0x00000006 System.Single Joystick::get_DeadZone()
+extern void Joystick_get_DeadZone_mCE52B635A8CF24F6DD867C14E34094515DE6AEFC (void);
+// 0x00000007 System.Void Joystick::set_DeadZone(System.Single)
+extern void Joystick_set_DeadZone_mD5699A14E5284026F303C8AF8D3457DFA9920F19 (void);
+// 0x00000008 AxisOptions Joystick::get_AxisOptions()
+extern void Joystick_get_AxisOptions_mA74F5FEE31C158E5179F0B108272ED28A661E388 (void);
+// 0x00000009 System.Void Joystick::set_AxisOptions(AxisOptions)
+extern void Joystick_set_AxisOptions_m541692280806ECF76F7C2C710973AF9D8AB334C6 (void);
+// 0x0000000A System.Boolean Joystick::get_SnapX()
+extern void Joystick_get_SnapX_m51CAFDCC399606BA82986908700AAA45668A0F40 (void);
+// 0x0000000B System.Void Joystick::set_SnapX(System.Boolean)
+extern void Joystick_set_SnapX_mB2090989F6AC933B30823751D74E799FC8D9B87A (void);
+// 0x0000000C System.Boolean Joystick::get_SnapY()
+extern void Joystick_get_SnapY_m35AFC1AD1DF5EDE5FCE8BAFEBE91AD51D7451613 (void);
+// 0x0000000D System.Void Joystick::set_SnapY(System.Boolean)
+extern void Joystick_set_SnapY_m7419D5EB972285A9E5E446CD668BEC266D11CD86 (void);
+// 0x0000000E System.Void Joystick::Start()
+extern void Joystick_Start_m5E46F090910AB69BE9838BFDB91A4F6E6934480C (void);
+// 0x0000000F System.Void Joystick::OnPointerDown(UnityEngine.EventSystems.PointerEventData)
+extern void Joystick_OnPointerDown_mF176903D532D9129C90BBBD00FD7714BA3A0D8E6 (void);
+// 0x00000010 System.Void Joystick::OnDrag(UnityEngine.EventSystems.PointerEventData)
+extern void Joystick_OnDrag_m39E69636AEDC0E471EAD1371A99F4694ECDBA1E9 (void);
+// 0x00000011 System.Void Joystick::HandleInput(System.Single,UnityEngine.Vector2,UnityEngine.Vector2,UnityEngine.Camera)
+extern void Joystick_HandleInput_m15A4E86369A1AF0A4A5727DEC0FD93212A99901C (void);
+// 0x00000012 System.Void Joystick::FormatInput()
+extern void Joystick_FormatInput_mDDF7AF40138CF227F0106811C8749180FBF45342 (void);
+// 0x00000013 System.Single Joystick::SnapFloat(System.Single,AxisOptions)
+extern void Joystick_SnapFloat_mADE5AF21C67A2298A08CD12F9A8ED73AFA866987 (void);
+// 0x00000014 System.Void Joystick::OnPointerUp(UnityEngine.EventSystems.PointerEventData)
+extern void Joystick_OnPointerUp_mEDED4DA77D954CBAC11CF82B57AB7A4DBFCDE22C (void);
+// 0x00000015 UnityEngine.Vector2 Joystick::ScreenPointToAnchoredPosition(UnityEngine.Vector2)
+extern void Joystick_ScreenPointToAnchoredPosition_mC1EB7560E844BF682674E4E7BD640604BC12B024 (void);
+// 0x00000016 System.Void Joystick::.ctor()
+extern void Joystick__ctor_m9BBE494CA4714F24227F33CB54C10B4DA78BF06B (void);
+// 0x00000017 System.Single DynamicJoystick::get_MoveThreshold()
+extern void DynamicJoystick_get_MoveThreshold_m16C670C1DA0A45E83F6F87C4304F459EDDEEDD5A (void);
+// 0x00000018 System.Void DynamicJoystick::set_MoveThreshold(System.Single)
+extern void DynamicJoystick_set_MoveThreshold_m000C1AE325C0B9C33172E4202F2AFB59820517F9 (void);
+// 0x00000019 System.Void DynamicJoystick::Start()
+extern void DynamicJoystick_Start_mFE16C6CE0B753F08E79A2AEC75782DEEF3B96F72 (void);
+// 0x0000001A System.Void DynamicJoystick::OnPointerDown(UnityEngine.EventSystems.PointerEventData)
+extern void DynamicJoystick_OnPointerDown_mBFA3026A0DA4A6B53C0E747A1E892CBC7F43E136 (void);
+// 0x0000001B System.Void DynamicJoystick::OnPointerUp(UnityEngine.EventSystems.PointerEventData)
+extern void DynamicJoystick_OnPointerUp_m10389907A9D3362F6B75FDC5F35AF37A5DD5AE7C (void);
+// 0x0000001C System.Void DynamicJoystick::HandleInput(System.Single,UnityEngine.Vector2,UnityEngine.Vector2,UnityEngine.Camera)
+extern void DynamicJoystick_HandleInput_m3F157F4825BE6682228C8E135581C6404D268506 (void);
+// 0x0000001D System.Void DynamicJoystick::.ctor()
+extern void DynamicJoystick__ctor_m9DDA6263314BD7B97679DF14A4664358BD3E58CB (void);
+// 0x0000001E System.Void FixedJoystick::.ctor()
+extern void FixedJoystick__ctor_m8C8BB74E5EA8CA2C3DD2AE084301EC91F519AD24 (void);
+// 0x0000001F System.Void FloatingJoystick::Start()
+extern void FloatingJoystick_Start_mB22059CD82AF77A8F94AC72E81A8BAE969399E81 (void);
+// 0x00000020 System.Void FloatingJoystick::OnPointerDown(UnityEngine.EventSystems.PointerEventData)
+extern void FloatingJoystick_OnPointerDown_mFE5B4F54D5BBCA72F9729AB64765F558FA5C7A54 (void);
+// 0x00000021 System.Void FloatingJoystick::OnPointerUp(UnityEngine.EventSystems.PointerEventData)
+extern void FloatingJoystick_OnPointerUp_m80ABA9C914E1953F91DBF74853CE84879352280D (void);
+// 0x00000022 System.Void FloatingJoystick::.ctor()
+extern void FloatingJoystick__ctor_m6B72425996D46B025F9E9D22121E9D01BEC6BD3C (void);
+// 0x00000023 System.Single VariableJoystick::get_MoveThreshold()
+extern void VariableJoystick_get_MoveThreshold_m8C9D3A63DB3B6CF1F0139C3504EC2E7AC4E7CF99 (void);
+// 0x00000024 System.Void VariableJoystick::set_MoveThreshold(System.Single)
+extern void VariableJoystick_set_MoveThreshold_m23DC4187B405EB690D297379E2113568B40C705A (void);
+// 0x00000025 System.Void VariableJoystick::SetMode(JoystickType)
+extern void VariableJoystick_SetMode_mB9D0B9B6E2E4F431E36AED6F5AC989305ADDB1EE (void);
+// 0x00000026 System.Void VariableJoystick::Start()
+extern void VariableJoystick_Start_m21743760641EA0317ACCD95949B99825446FE74D (void);
+// 0x00000027 System.Void VariableJoystick::OnPointerDown(UnityEngine.EventSystems.PointerEventData)
+extern void VariableJoystick_OnPointerDown_m8ABE5C8EFBC8DB3A2EE135FFF3C0F67C533AF4B5 (void);
+// 0x00000028 System.Void VariableJoystick::OnPointerUp(UnityEngine.EventSystems.PointerEventData)
+extern void VariableJoystick_OnPointerUp_m65296D82A6C2E1BDC2D622B9C922FAE8E4544526 (void);
+// 0x00000029 System.Void VariableJoystick::HandleInput(System.Single,UnityEngine.Vector2,UnityEngine.Vector2,UnityEngine.Camera)
+extern void VariableJoystick_HandleInput_mD1BCF9A068737A9C057EE8CEB7E6DEB682CC03AB (void);
+// 0x0000002A System.Void VariableJoystick::.ctor()
+extern void VariableJoystick__ctor_m6C7B41973BE20A94F16DB5DCC9AA805C5D8DF852 (void);
+// 0x0000002B System.Void BackToMenu::OnTriggerEnter(UnityEngine.Collider)
+extern void BackToMenu_OnTriggerEnter_m6B286EC25AEA48AFF5EC0B0A89C19A6A3D21F8AA (void);
+// 0x0000002C System.Void BackToMenu::.ctor()
+extern void BackToMenu__ctor_mB80D7C69D6AF7F7B88AEF2F34345D2CF310127A9 (void);
+// 0x0000002D System.Void Bullet::Start()
+extern void Bullet_Start_m6BD187DD353835D248DA404B169DCE29CEB2B813 (void);
+// 0x0000002E System.Void Bullet::Update()
+extern void Bullet_Update_m5AA63D0B1F389C2CFEE77466E1C39ADC813B4DBC (void);
+// 0x0000002F System.Void Bullet::OnTriggerEnter(UnityEngine.Collider)
+extern void Bullet_OnTriggerEnter_m0EFCBDB1D5C31549CEA14C46C869D823DE1D18D8 (void);
+// 0x00000030 System.Void Bullet::.ctor()
+extern void Bullet__ctor_m873C02F2114EA93A35E4392013AC831246756CBA (void);
+// 0x00000031 System.Void Enemy::Start()
+extern void Enemy_Start_m8BBD9A5AE10A27ABDFCD9168B93CD9C69D229034 (void);
+// 0x00000032 System.Void Enemy::Update()
+extern void Enemy_Update_m4149CFC3AC081AF0D654D9BDB6BC9B5540CE03D8 (void);
+// 0x00000033 System.Void Enemy::OnTriggerEnter(UnityEngine.Collider)
+extern void Enemy_OnTriggerEnter_m46AB50D33A3CEA54D6DF6FCF4A5AAFE0C7A60B4B (void);
+// 0x00000034 System.Void Enemy::.ctor()
+extern void Enemy__ctor_mB6697627910F785A971C20C671DEFBA9D921D933 (void);
+// 0x00000035 System.Void GameManager::StartGame()
+extern void GameManager_StartGame_m2CAC97E2C33DD5FF5CF10FAF17313080A85464B7 (void);
+// 0x00000036 System.Void GameManager::QuitGame()
+extern void GameManager_QuitGame_m84BA17826FA89C594024D63671F626499DF12D69 (void);
+// 0x00000037 System.Void GameManager::.ctor()
+extern void GameManager__ctor_mF453CED520617BFB65C52405A964E06CF17DB368 (void);
+// 0x00000038 System.Void KeyItem::OnTriggerEnter(UnityEngine.Collider)
+extern void KeyItem_OnTriggerEnter_mDEF26EE54135E806F976FC847E75CD690E513333 (void);
+// 0x00000039 System.Void KeyItem::.ctor()
+extern void KeyItem__ctor_mD5FF1956C071A4E04AA085FC31BD843CBC08E30B (void);
+// 0x0000003A System.Void Player::Start()
+extern void Player_Start_mC31CF4F40DDEA35C5E39E8C43EC37284AE7C453D (void);
+// 0x0000003B System.Void Player::Update()
+extern void Player_Update_m95E134A5EF1B5164EA281F61D7FA436F59BE3C9F (void);
+// 0x0000003C System.Void Player::Fire()
+extern void Player_Fire_mDE0B94065A6562B1D49DE20E06496B0182043E18 (void);
+// 0x0000003D System.Collections.IEnumerator Player::KeepShooting()
+extern void Player_KeepShooting_m4D7CD4359DD1B2EB2D50EF586F1C05C41C1264D2 (void);
+// 0x0000003E System.Void Player::.ctor()
+extern void Player__ctor_m0A83E0706592FC871B0CF188B37AFC6649F3D85D (void);
+// 0x0000003F System.Void Player/<KeepShooting>d__9::.ctor(System.Int32)
+extern void U3CKeepShootingU3Ed__9__ctor_mFB681125A89D51148D31696952088E8117DBA182 (void);
+// 0x00000040 System.Void Player/<KeepShooting>d__9::System.IDisposable.Dispose()
+extern void U3CKeepShootingU3Ed__9_System_IDisposable_Dispose_mC5AAFB5FB756FB52AB8631728DFBE97891B4678A (void);
+// 0x00000041 System.Boolean Player/<KeepShooting>d__9::MoveNext()
+extern void U3CKeepShootingU3Ed__9_MoveNext_m95B1B85AC02276BD99D2D8DB7A9F482577D21713 (void);
+// 0x00000042 System.Object Player/<KeepShooting>d__9::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CKeepShootingU3Ed__9_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m2207B8C269DA64E2B46D5A2AD707E007DE85B0A6 (void);
+// 0x00000043 System.Void Player/<KeepShooting>d__9::System.Collections.IEnumerator.Reset()
+extern void U3CKeepShootingU3Ed__9_System_Collections_IEnumerator_Reset_m9C570DB8184697DC7B0FD5DF21445C092BAD7E69 (void);
+// 0x00000044 System.Object Player/<KeepShooting>d__9::System.Collections.IEnumerator.get_Current()
+extern void U3CKeepShootingU3Ed__9_System_Collections_IEnumerator_get_Current_mF43F8566703D84A6035451711130599179A7BAE0 (void);
+// 0x00000045 System.Void SceneSwitch::OnTriggerEnter(UnityEngine.Collider)
+extern void SceneSwitch_OnTriggerEnter_mAB129706564136FBD8C3E6A6157362C23F509D7E (void);
+// 0x00000046 System.Void SceneSwitch::.ctor()
+extern void SceneSwitch__ctor_mD1B7AE904BCF221FA36B02267BB1B97B297259AD (void);
+// 0x00000047 System.Void Treasure::OnTriggerEnter(UnityEngine.Collider)
+extern void Treasure_OnTriggerEnter_m5BCD8C11C0DB43C93F8C21F223115839E550B253 (void);
+// 0x00000048 System.Void Treasure::.ctor()
+extern void Treasure__ctor_m1DA17FC6BB095DCAB4AE16C6FF305E36CFC35B9A (void);
+static Il2CppMethodPointer s_methodPointers[72] = 
+{
+	Joystick_get_Horizontal_m78CF4472B86063E54254AC8AE0A52126E4008AFA,
+	Joystick_get_Vertical_mA2B0917896CF9CE47A6D342D1734E43441C3D4BE,
+	Joystick_get_Direction_m52502695D42BDAB6075089BDE442ABE72EAC81EC,
+	Joystick_get_HandleRange_mB38F0D3B6ADE2D1557D7A3D6759C469F17509D77,
+	Joystick_set_HandleRange_m686B579A1F02EFCD4878BEA27EA606FC23CD2505,
+	Joystick_get_DeadZone_mCE52B635A8CF24F6DD867C14E34094515DE6AEFC,
+	Joystick_set_DeadZone_mD5699A14E5284026F303C8AF8D3457DFA9920F19,
+	Joystick_get_AxisOptions_mA74F5FEE31C158E5179F0B108272ED28A661E388,
+	Joystick_set_AxisOptions_m541692280806ECF76F7C2C710973AF9D8AB334C6,
+	Joystick_get_SnapX_m51CAFDCC399606BA82986908700AAA45668A0F40,
+	Joystick_set_SnapX_mB2090989F6AC933B30823751D74E799FC8D9B87A,
+	Joystick_get_SnapY_m35AFC1AD1DF5EDE5FCE8BAFEBE91AD51D7451613,
+	Joystick_set_SnapY_m7419D5EB972285A9E5E446CD668BEC266D11CD86,
+	Joystick_Start_m5E46F090910AB69BE9838BFDB91A4F6E6934480C,
+	Joystick_OnPointerDown_mF176903D532D9129C90BBBD00FD7714BA3A0D8E6,
+	Joystick_OnDrag_m39E69636AEDC0E471EAD1371A99F4694ECDBA1E9,
+	Joystick_HandleInput_m15A4E86369A1AF0A4A5727DEC0FD93212A99901C,
+	Joystick_FormatInput_mDDF7AF40138CF227F0106811C8749180FBF45342,
+	Joystick_SnapFloat_mADE5AF21C67A2298A08CD12F9A8ED73AFA866987,
+	Joystick_OnPointerUp_mEDED4DA77D954CBAC11CF82B57AB7A4DBFCDE22C,
+	Joystick_ScreenPointToAnchoredPosition_mC1EB7560E844BF682674E4E7BD640604BC12B024,
+	Joystick__ctor_m9BBE494CA4714F24227F33CB54C10B4DA78BF06B,
+	DynamicJoystick_get_MoveThreshold_m16C670C1DA0A45E83F6F87C4304F459EDDEEDD5A,
+	DynamicJoystick_set_MoveThreshold_m000C1AE325C0B9C33172E4202F2AFB59820517F9,
+	DynamicJoystick_Start_mFE16C6CE0B753F08E79A2AEC75782DEEF3B96F72,
+	DynamicJoystick_OnPointerDown_mBFA3026A0DA4A6B53C0E747A1E892CBC7F43E136,
+	DynamicJoystick_OnPointerUp_m10389907A9D3362F6B75FDC5F35AF37A5DD5AE7C,
+	DynamicJoystick_HandleInput_m3F157F4825BE6682228C8E135581C6404D268506,
+	DynamicJoystick__ctor_m9DDA6263314BD7B97679DF14A4664358BD3E58CB,
+	FixedJoystick__ctor_m8C8BB74E5EA8CA2C3DD2AE084301EC91F519AD24,
+	FloatingJoystick_Start_mB22059CD82AF77A8F94AC72E81A8BAE969399E81,
+	FloatingJoystick_OnPointerDown_mFE5B4F54D5BBCA72F9729AB64765F558FA5C7A54,
+	FloatingJoystick_OnPointerUp_m80ABA9C914E1953F91DBF74853CE84879352280D,
+	FloatingJoystick__ctor_m6B72425996D46B025F9E9D22121E9D01BEC6BD3C,
+	VariableJoystick_get_MoveThreshold_m8C9D3A63DB3B6CF1F0139C3504EC2E7AC4E7CF99,
+	VariableJoystick_set_MoveThreshold_m23DC4187B405EB690D297379E2113568B40C705A,
+	VariableJoystick_SetMode_mB9D0B9B6E2E4F431E36AED6F5AC989305ADDB1EE,
+	VariableJoystick_Start_m21743760641EA0317ACCD95949B99825446FE74D,
+	VariableJoystick_OnPointerDown_m8ABE5C8EFBC8DB3A2EE135FFF3C0F67C533AF4B5,
+	VariableJoystick_OnPointerUp_m65296D82A6C2E1BDC2D622B9C922FAE8E4544526,
+	VariableJoystick_HandleInput_mD1BCF9A068737A9C057EE8CEB7E6DEB682CC03AB,
+	VariableJoystick__ctor_m6C7B41973BE20A94F16DB5DCC9AA805C5D8DF852,
+	BackToMenu_OnTriggerEnter_m6B286EC25AEA48AFF5EC0B0A89C19A6A3D21F8AA,
+	BackToMenu__ctor_mB80D7C69D6AF7F7B88AEF2F34345D2CF310127A9,
+	Bullet_Start_m6BD187DD353835D248DA404B169DCE29CEB2B813,
+	Bullet_Update_m5AA63D0B1F389C2CFEE77466E1C39ADC813B4DBC,
+	Bullet_OnTriggerEnter_m0EFCBDB1D5C31549CEA14C46C869D823DE1D18D8,
+	Bullet__ctor_m873C02F2114EA93A35E4392013AC831246756CBA,
+	Enemy_Start_m8BBD9A5AE10A27ABDFCD9168B93CD9C69D229034,
+	Enemy_Update_m4149CFC3AC081AF0D654D9BDB6BC9B5540CE03D8,
+	Enemy_OnTriggerEnter_m46AB50D33A3CEA54D6DF6FCF4A5AAFE0C7A60B4B,
+	Enemy__ctor_mB6697627910F785A971C20C671DEFBA9D921D933,
+	GameManager_StartGame_m2CAC97E2C33DD5FF5CF10FAF17313080A85464B7,
+	GameManager_QuitGame_m84BA17826FA89C594024D63671F626499DF12D69,
+	GameManager__ctor_mF453CED520617BFB65C52405A964E06CF17DB368,
+	KeyItem_OnTriggerEnter_mDEF26EE54135E806F976FC847E75CD690E513333,
+	KeyItem__ctor_mD5FF1956C071A4E04AA085FC31BD843CBC08E30B,
+	Player_Start_mC31CF4F40DDEA35C5E39E8C43EC37284AE7C453D,
+	Player_Update_m95E134A5EF1B5164EA281F61D7FA436F59BE3C9F,
+	Player_Fire_mDE0B94065A6562B1D49DE20E06496B0182043E18,
+	Player_KeepShooting_m4D7CD4359DD1B2EB2D50EF586F1C05C41C1264D2,
+	Player__ctor_m0A83E0706592FC871B0CF188B37AFC6649F3D85D,
+	U3CKeepShootingU3Ed__9__ctor_mFB681125A89D51148D31696952088E8117DBA182,
+	U3CKeepShootingU3Ed__9_System_IDisposable_Dispose_mC5AAFB5FB756FB52AB8631728DFBE97891B4678A,
+	U3CKeepShootingU3Ed__9_MoveNext_m95B1B85AC02276BD99D2D8DB7A9F482577D21713,
+	U3CKeepShootingU3Ed__9_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m2207B8C269DA64E2B46D5A2AD707E007DE85B0A6,
+	U3CKeepShootingU3Ed__9_System_Collections_IEnumerator_Reset_m9C570DB8184697DC7B0FD5DF21445C092BAD7E69,
+	U3CKeepShootingU3Ed__9_System_Collections_IEnumerator_get_Current_mF43F8566703D84A6035451711130599179A7BAE0,
+	SceneSwitch_OnTriggerEnter_mAB129706564136FBD8C3E6A6157362C23F509D7E,
+	SceneSwitch__ctor_mD1B7AE904BCF221FA36B02267BB1B97B297259AD,
+	Treasure_OnTriggerEnter_m5BCD8C11C0DB43C93F8C21F223115839E550B253,
+	Treasure__ctor_m1DA17FC6BB095DCAB4AE16C6FF305E36CFC35B9A,
+};
+static const int32_t s_InvokerIndices[72] = 
+{
+	3157,
+	3157,
+	3194,
+	3157,
+	2657,
+	3157,
+	2657,
+	3103,
+	2610,
+	3066,
+	2572,
+	3066,
+	2572,
+	3203,
+	2627,
+	2627,
+	576,
+	3203,
+	1179,
+	2627,
+	2430,
+	3203,
+	3157,
+	2657,
+	3203,
+	2627,
+	2627,
+	576,
+	3203,
+	3203,
+	3203,
+	2627,
+	2627,
+	3203,
+	3157,
+	2657,
+	2610,
+	3203,
+	2627,
+	2627,
+	576,
+	3203,
+	2627,
+	3203,
+	3203,
+	3203,
+	2627,
+	3203,
+	3203,
+	3203,
+	2627,
+	3203,
+	3203,
+	3203,
+	3203,
+	2627,
+	3203,
+	3203,
+	3203,
+	3203,
+	3122,
+	3203,
+	2610,
+	3203,
+	3066,
+	3122,
+	3203,
+	3122,
+	2627,
+	3203,
+	2627,
+	3203,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	72,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
